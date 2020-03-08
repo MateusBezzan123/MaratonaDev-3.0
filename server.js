f1f9977a3126f222a486a7cdacdc45840cca6971
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2020-03-08 19:22:54 -0300

    começando o servidor

--- server.js
+++ server.js
@@ -1,26 +1,51 @@
-// logica de fazer um café
-const cor = "branco"
-const tamanho = 2.5
+const express = require("express")
+const server = express()
 
-function verificarSeOCopoEstaSujo(sujo){
+server.listen(3000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// logica de fazer um café
+//const cor = "branco"
+//const tamanho = 2.5
+//function verificarSeOCopoEstaSujo(sujo){
     //logica para verificar se o copo está sujo
     //return "está sujo"
     //return "não está sujo"
-    return `o copo ${sujo}`
-}
-
-const copo = {
+    //return `o copo ${sujo}`
+//}
+//const copo = {
     //cor:"branco"
-    cor,
-    tamanho,
-    verificarSeOCopoEstaSujo,
+    // cor,
+    //tamanho,
+    //verificarSeOCopoEstaSujo,
     //verificarSeOCopoEstaSujo(){},
-}
-
-console.log(copo.verificarSeOCopoEstaSujo("está sujo"))
-console.log(copo.verificarSeOCopoEstaSujo("não está sujo"))
-console.log(copo.verificarSeOCopoEstaSujo("não está sujo"))
-console.log(copo.verificarSeOCopoEstaSujo("está sujo"))
-console.log(copo.verificarSeOCopoEstaSujo("não está sujo"))
+//}
+//console.log(copo.verificarSeOCopoEstaSujo("está sujo"))
+//console.log(copo.verificarSeOCopoEstaSujo("não está sujo"))
+//console.log(copo.verificarSeOCopoEstaSujo("não está sujo"))
+//console.log(copo.verificarSeOCopoEstaSujo("está sujo"))
+//console.log(copo.verificarSeOCopoEstaSujo("não está sujo"))
 //console.log(copo.verificarSeOCopoEstaSujo())
 //copo.verificarSeOCopoEstaSujo()
