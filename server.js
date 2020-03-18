1287bcac411899134f4ca712c3f6f98466ee4f15
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2020-03-18 19:42:13 -0300

    Deu pau nesse projeto

--- server.js
+++ server.js
@@ -1,47 +1,73 @@
-//configurando o servidor
+// configurando o servidor
 const express = require("express")
 const server = express()
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+server.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+server.use(bodyParser.json())
 
 //configurar o servidor para  apresentar arquivos estáticos
 server.use(express.static('public'))
 
+// habilitar body do formulario
+server.use(express.urlencoded({ extended: true }))
 
-//configurando a template engine
+// configurando a template engine
 const nunjucks = require("nunjucks")
+
 nunjucks.configure("./",{
     express: server,
     noCache:true, //boolean ou booleano aceita 2 valores,verdadiero ou falso
 })
 
-//Lista de doadores: Vetor ou Array
+// Lista de doadores: Vetor ou Array
 const donors = [
     {
         name:"Diego Fernandes",
         blood:"AB+"
     },
+    
     {
         name:"Cleiton Souza",
         blood:"B+"
     },
+    
     {
         name:"Robson Marques",
         blood:"O+"
     },
+    
     {
         name:"Mayk Brito",
         blood:"A-"
     },
 ]
 
-//configurar a apresentação da pagina
+// configurar a apresentação da pagina
 server.get("/", function(req,res){
-    return res.render("index.html",{ donors })
-})
-server.post("/", function(res,req){
-    
+    return res.render("index.html", { donors })
 })
 
-//ligar o servidor e permitir o acesso na porta 3000
+server.post("/", function(res,req){
+    
+    // pegar dados do formulario
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+    
+    // coloca valores dentro do array
+    donors.push({
+        name:name,
+        blood:blood
+    })
+
+    return res.redirect("/")
+})
+
+// ligar o servidor e permitir o acesso na porta 3000
 server.listen(3000,function(){
     console.log("iniciei o servidor")
 })
