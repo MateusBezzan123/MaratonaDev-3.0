ca3f0f06c259ee3d0c1dd89bf59f9ea6113bb9c6
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2020-03-18 16:03:25 -0300

    Ta indo

--- server.js
+++ server.js
@@ -9,19 +9,33 @@ server.use(express.static('public'))
 //configurando a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./",{
-    express: server
+    express: server,
+    noCache:true, //boolean ou booleano aceita 2 valores,verdadiero ou falso
 })
 
+//Lista de doadores: Vetor ou Array
 const donors = [
     {
         name:"Diego Fernandes",
-        blood:"AB"
-    }
+        blood:"AB+"
+    },
+    {
+        name:"Cleiton Souza",
+        blood:"B+"
+    },
+    {
+        name:"Robson Marques",
+        blood:"O+"
+    },
+    {
+        name:"Mayk Brito",
+        blood:"A-"
+    },
 ]
 
 //configurar a apresentação da pagina
 server.get("/", function(req,res){
-    return res.render("index.html")
+    return res.render("index.html",{ donors })
 })
 
 //ligar o servidor e permitir o acesso na porta 3000
